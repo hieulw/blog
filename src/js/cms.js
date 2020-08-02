48008f7aa25534fa3444d91b58d8e504ebef7162
ladyga14 <murindeventa@gmail.com>
2020-08-02 12:09:56 +0700

    :wrench: Remove preview and modify title

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,13 +1,13 @@
-import React from "react";
-import CMS from "netlify-cms-app";
+// import React from "react";
+// import CMS from "netlify-cms-app";
 
-// Import main site styles as a string to inject into the CMS preview pane
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
+// // Import main site styles as a string to inject into the CMS preview pane
+// import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
-import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
+// import HomePreview from "./cms-preview-templates/home";
+// import PostPreview from "./cms-preview-templates/post";
 
-CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewTemplate("home", HomePreview);
-CMS.registerPreviewTemplate("posts", PostPreview);
-CMS.init();
+// CMS.registerPreviewStyle(styles, { raw: true });
+// CMS.registerPreviewTemplate("home", HomePreview);
+// CMS.registerPreviewTemplate("posts", PostPreview);
+// CMS.init();
