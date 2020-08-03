9988f0e604fcf3c4be611377bb94be0b597747f8
ladyga14 <murindeventa@gmail.com>
2020-08-03 10:17:05 +0700

    :bug: Fix blank admin page

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,5 +1,5 @@
 // import React from "react";
-// import CMS from "netlify-cms-app";
+import CMS from "netlify-cms-app";
 
 // // Import main site styles as a string to inject into the CMS preview pane
 // import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
@@ -10,4 +10,4 @@
 // CMS.registerPreviewStyle(styles, { raw: true });
 // CMS.registerPreviewTemplate("home", HomePreview);
 // CMS.registerPreviewTemplate("posts", PostPreview);
-// CMS.init();
+CMS.init();
